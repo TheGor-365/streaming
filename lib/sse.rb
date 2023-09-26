56d0f1652a8db6eec253172319f738e731f9e45c
Gor <7495500@gmail.com>
2023-09-26 21:53:28 +0300

    Add minor changes 2

--- lib/sse.rb
+++ lib/sse.rb
@@ -1,21 +1,19 @@
 require 'json'
 
-module ServerSide
-  class SSE
-    def initialize io
-      @io = io
+class SSE
+  def initialize(io)
+    @io = io
+  end
+
+  def write(object, options = {})
+    options.each do |name, value|
+      @io.write "#{name}: #{value}\n"
     end
 
-    def write object, options = {}
-      options.each do |name, value|
-        @io.write "#{name}: #{value}\n"
-      end
+    @io.write "data: #{object}\n\n"
+  end
 
-      @io.write "data: #{object}\n\n"
-    end
-
-    def close
-      @io.close
-    end
+  def close
+    @io.close
   end
 end
