7ebf3c70dd0fbe1277849a23635250e7cddc090f
Gor <7495500@gmail.com>
2023-09-26 21:51:10 +0300

    Add minor changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,23 +4,31 @@ class WelcomeController < ApplicationController
   include ActionController::Live
 
   def homepage
-    data = 'hey'
+    # data = 'hey'
     # send_data(data, filename: 'hey.txt')
     # send_data(File.open(Rails.root.join('public', '404.html')), w { |file| file << data }, filename: '404.html')
-    send_file(Rails.root.join('public', '404.html'), filename: '404.html')
+    # send_file(Rails.root.join('public', '404.html'), filename: '404.html')
   end
 
   def send_message
     response.headers['Content-Type'] = 'text/event-stream'
-    sse = ServerSide::SSE.new(response.stream)
+    # @message = params[:message]
+
+    # sse = SSE.new(response.stream)
 
     begin
       loop do
-        sse.write({ message: "#{params[:message]}" })
+        sse.write({ message: "#{ params[:message]}" })
       end
     rescue IOError
     ensure
       sse.close
     end
-  end
+
+  #   10.times do
+  #     response.stream.write "#{ @message }\n"
+  #   end
+  # ensure
+  #   response.stream.close
+  # end
 end
