ba3678b876cdf30418e6ed803de9e749d4774c2c
Gor <7495500@gmail.com>
2023-09-27 12:24:34 +0300

    Change send_file snippet

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,24 +6,24 @@ class WelcomeController < ApplicationController
   def homepage
     # data = 'hey'
     # send_data(data, filename: 'hey.txt')
-    # send_data(File.open(Rails.root.join('public', '404.html')), w { |file| file << data }, filename: '404.html')
-    # send_file(Rails.root.join('public', '404.html'), filename: '404.html')
+    send_file(Rails.root.join('public', '404.html'), filename: '404.html')
   end
 
-  def send_message
-    response.headers['Content-Type'] = 'text/event-stream'
+  # def send_message
+    # response.headers['Content-Type'] = 'text/event-stream'
     # @message = params[:message]
 
     # sse = SSE.new(response.stream)
 
-    begin
-      loop do
-        sse.write({ message: "#{ params[:message]}" })
-      end
-    rescue IOError
-    ensure
-      sse.close
-    end
+    # begin
+    #   loop do
+    #     sse.write({ message: "#{ @message }" })
+    #   end
+    # rescue IOError
+    # ensure
+    #   sse.close
+    #   response.stream.close
+    # end
 
   #   10.times do
   #     response.stream.write "#{ @message }\n"
